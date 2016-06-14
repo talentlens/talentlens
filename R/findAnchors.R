85202056a6629b252c114c1925c390c960a32297
talentlens <morgan.strom@pearson.com>
2016-06-14 10:37:45 +0100

    Edit function findAnchors

--- R/findAnchors.R
+++ R/findAnchors.R
@@ -39,7 +39,29 @@ findAnchors <- function(score_matrix, model = c("rasch", "2pl"),
   drop$low_cor <- cor[low] #Items with low item-total correlations
 
   #Drop bad items
-  score_matrix_ <- score_matrix[, !low & !easy & !hard]
+  score_matrix_ <- score_matrix[, !(low | easy | hard)]
+
+  #Print message to console
+  if (verbose) {
+
+    cat(paste("Dropping", sum(!(low | easy | hard)), "items\n"))
+
+    if(length(drop$easy > 0)) {
+      cat(paste("\nToo easy (prop correct >", max_p, "):\n"))
+      for (i in 1:length(drop$easy)) cat(paste0("\t", names(drop$easy)[i], ": ", round(drop$easy[i], 2), "\n"))
+    }
+
+    if(length(drop$hard > 0)) {
+      cat(paste("\nToo hard (prop correct <", min_p, "):\n"))
+      for (i in 1:length(drop$hard)) cat(paste0("\t", names(drop$hard)[i], ": ", round(drop$hard[i], 2), "\n"))
+    }
+
+    if(length(drop$low_cor > 0)) {
+      cat(paste("\nLow item-total correlation (biserial r <", min_cor, "):\n"))
+      for (i in 1:length(drop$low_cor)) cat(paste0("\t", names(drop$low_cor)[i], ": ", round(drop$low_cor[i], 2), "\n"))
+    }
+
+  }
 
 
   #IRT analyses -------------------------------
@@ -56,6 +78,7 @@ findAnchors <- function(score_matrix, model = c("rasch", "2pl"),
 
     #Refit model
     score_matrix_ <- score_matrix_[,!bad_fit]
+    if (verbose) cat("\nFitting IRT parameters:\n")
     fit <- ltm::rasch(score_matrix_, IRT.param = TRUE,
                       control = list(iter.qN = 150, GHk = 21, verbose = verbose))
 
@@ -71,6 +94,7 @@ findAnchors <- function(score_matrix, model = c("rasch", "2pl"),
 
     #Refit model
     score_matrix_ <- score_matrix_[,!bad_fit]
+    if (verbose) cat("\nFitting IRT parameters:\n")
     fit <- ltm::ltm(formula = score_matrix_ ~ z1, IRT.param = TRUE,
                     control = list(iter.em = 40, GHk = 21, verbose = verbose))
 
@@ -83,8 +107,9 @@ findAnchors <- function(score_matrix, model = c("rasch", "2pl"),
   #Return optimal anchor items -------------------
   anchors <- coefficients(fit)
 
-  if (verbose) {
-    print(drop)
+  if (verbose & length(drop$bad_fit > 0)) {
+    cat("\nDropping items because of bad item fit (chi-square test p < 0.05):\n")
+    for(i in 1:length(drop$bad_fit)) cat(paste0("\t", names(drop$bad_fit)[i], "\n"))
   }
 
   return(anchors)
